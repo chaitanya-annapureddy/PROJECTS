2d0b44369b4a2c17f60549feac1d62d7401dab6f
Chaitanya annapureddy <chaitanyaannapureddy206@gmail.com>
2026-03-29 12:04:59 +0530

    Create rd_sqr.sv

---
+++ ASYN_FIFO/rd_agt/rd_sqr.sv
@@ -0,0 +1,10 @@
+
+       class rd_sqr extends uvm_sequencer #(rd_txn);
+          `uvm_component_utils(rd_sqr)
+
+           function new(string name="rd_sqr",uvm_component parent);
+                super.new(name,parent);
+           endfunction
+
+        endclass
+~

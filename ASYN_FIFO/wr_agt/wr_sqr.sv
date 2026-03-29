b8ec90960451ad4165709204357ad78e0aca6a30
Chaitanya annapureddy <chaitanyaannapureddy206@gmail.com>
2026-03-29 11:50:36 +0530

    Create wr_sqr.sv

---
+++ ASYN_FIFO/wr_agt/wr_sqr.sv
@@ -0,0 +1,11 @@
+ class wr_sqr extends uvm_sequencer #(wr_txn);
+          `uvm_component_utils(wr_sqr)
+
+           function new(string name="wr_sqr",uvm_component parent);
+                super.new(name,parent);
+           endfunction
+
+        endclass
+
+~
+~
